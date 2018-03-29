90f4865b1214af2f5471b265b55d2e554aebda60
Abdullah Al-Hasnat <abdullahalhasnat@gmail.com>
2018-03-29 19:51:52 +0600

    Chapter 1 Compelete

--- src/part_one/chapter_1/MainThreadExample.java
+++ src/part_one/chapter_1/MainThreadExample.java
@@ -4,5 +4,8 @@ public class MainThreadExample {
 
     public static void main(String[] args) {
 
+        Thread current_thread = Thread.currentThread();
+
+        System.out.println("Current Thread Name: "+ current_thread.getName());
     }
 }
